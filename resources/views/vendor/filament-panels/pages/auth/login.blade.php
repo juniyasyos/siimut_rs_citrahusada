96fc61306f3d2d4e24c7bf313a0087b840af84f8
juniyasyos <juniyasyos@gmail.com>
2025-03-23 23:11:16 +0700

    publish asset filament and lang

--- resources/views/vendor/filament-panels/pages/auth/login.blade.php
+++ resources/views/vendor/filament-panels/pages/auth/login.blade.php
@@ -12,10 +12,7 @@
     <x-filament-panels::form id="form" wire:submit="authenticate">
         {{ $this->form }}
 
-        <x-filament-panels::form.actions
-            :actions="$this->getCachedFormActions()"
-            :full-width="$this->hasFullWidthFormActions()"
-        />
+        <x-filament-panels::form.actions :actions="$this->getCachedFormActions()" :full-width="$this->hasFullWidthFormActions()" />
     </x-filament-panels::form>
 
     {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER, scopes: $this->getRenderHookScopes()) }}
